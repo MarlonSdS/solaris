67dc7f0d5bfe9ee0417fc053706200c610451662
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2020-11-03 10:24:52 -0300

    Estrutura Inicial
    Adicionados pacotes e classes que serão a base para a construção do projeto. Sujeito a mudanças no futuro

--- src/objetos/Usuario.java
+++ src/objetos/Usuario.java
@@ -3,12 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package displays;
+package objetos;
 
 /**
  *
  * @author Marlo
  */
-public class Main {
+public class Usuario {
     
+    public String nomeUsuario;
+    
+    public String emailUsuario;
+    
+    public String senhaUsuario;
 }
